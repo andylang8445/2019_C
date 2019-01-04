86861eeaebd29d929f810092a32bc2e3926b68e0
Hongjun Yun <andylang8445@gmail.com>
2019-01-04 14:48:55 -0500

    V 0.5 Movement added (Also works on English,Korean,Arrow Key) Added Beep when you try to push the wall Small Known Errors Fixed

--- Game_Project/Runn/Project1/Project1/main.cpp
+++ Game_Project/Runn/Project1/Project1/main.cpp
@@ -8,7 +8,10 @@
 #define COL                   GetStdHandle(STD_OUTPUT_HANDLE)        // 콘솔창의 핸들정보 받기
 #define height 38
 #define width 60
+#define original_Px 30
+#define original_Py 30
 int map[height][width];
+int ex;
 int Px, Py;
 void gotoxy(int xxx, int yyy)  //x,y순서로 입력, 커서 이동(배열 좌표아닌 실제 좌표) 
 {
@@ -38,8 +41,8 @@ void setup_seq()
 		map[0][i] = 1;
 		map[height - 1][i] = 1;
 	}
-	Px = 30;
-	Py = 30;
+	Px = original_Px;
+	Py = original_Py;
 
 	//------Basic Map Form print-------//
 	for (int i = 0; i < height; i++)
@@ -60,6 +63,8 @@ void run_game()
 }
 int main()
 {
+	int tmp;
+	int beep_l = 0, beep_r = 0, beep_u = 0, beep_d = 0;
 	system("mode con cols=60 lines=40");
 	system("title Run");
 	setup_seq();
@@ -67,7 +72,87 @@ int main()
 	printf("$");
 	while (1)
 	{
-		break;
+		if (_kbhit())
+		{
+			tmp = _getch();
+			if (tmp == 'w' || tmp == 'ㅈ')
+			{
+			up:
+				if (Py == 1)
+				{
+					if (beep_u % 10 == 0)
+						Beep(512, 200);
+					beep_u++;
+					goto end;
+				}
+				beep_l = beep_r = beep_d = beep_u = 0;
+				gotoxy(Px, Py);
+				printf(" ");
+				Py--;
+			}
+			if (tmp == 's' || tmp == 'ㄴ')
+			{
+			down:
+				if (Py == height - 2)
+				{
+					if (beep_d % 10 == 0)
+						Beep(512, 200);
+					beep_d++;
+					goto end;
+				}
+				beep_l = beep_r = beep_u = beep_d = 0;
+				gotoxy(Px, Py);
+				printf(" ");
+				Py++;
+			}
+			if (tmp == 'a' || tmp == 'ㅁ')
+			{
+			left:
+				if (Px == 1)
+				{
+					if (beep_l % 10 == 0)
+						Beep(512, 200);
+					beep_l++;
+					goto end;
+				}
+				beep_u = beep_r = beep_d = beep_l = 0;
+				gotoxy(Px, Py);
+				printf(" ");
+				Px--;
+			}
+			if (tmp == 'd' || tmp == 'ㅇ')
+			{
+			right:
+				if (Px == width - 2)
+				{
+					if (beep_r % 10 == 0)
+						Beep(512, 200);
+					beep_r++;
+					goto end;
+				}
+				beep_l = beep_u = beep_d = beep_r = 0;
+				gotoxy(Px, Py);
+				printf(" ");
+				Px++;
+			}
+			if (tmp == 224)
+			{
+				tmp = _getch();
+				if (tmp == 72)
+					goto up;
+				if (tmp == 80)
+					goto down;
+				if (tmp == 75)
+					goto left;
+				if (tmp == 77)
+					goto right;
+			}
+
+			gotoxy(Px, Py);
+			printf("$");
+		end:
+			continue;
+		}
 	}
 	_getch();
 }
