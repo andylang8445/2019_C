cbb3b7c3a5075448653f91d922f240279827269f
Hongjun Yun <andylang8445@gmail.com>
2019-01-03 22:46:19 -0500

    ccc 2011 Junior #1 "Which Alien?" Done

---
+++ 20190103/ccc 2011/ccc 2011/which Alien.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a, e;
+	scanf_s("%d %d", &a, &e);
+	if (a >= 3 && e <= 4)
+		printf("TroyMartian\n");
+	if (a <= 6 && e >= 2)
+		printf("VladSaturnian\n");
+	if (a <= 2 && e <= 3)
+		printf("GraemeMercurian");
+}
